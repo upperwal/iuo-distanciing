df5f7f53b548d7c142747585e6d2a52745e535d6
Abhishek Upperwal <a.upperwal@gmail.com>
2020-07-04 15:31:36 +0530

    Updates

---
+++ precache-manifest.fd87805d0c43cf6632b36ec03f30d10b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086283636927812c38ba062e0fba4f9a",
+    "url": "/iuo-distancing/index.html"
+  },
+  {
+    "revision": "5a477669a839f2f61a00",
+    "url": "/iuo-distancing/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "824ec06fbd51b80edacf",
+    "url": "/iuo-distancing/static/css/main.53c98df2.chunk.css"
+  },
+  {
+    "revision": "5a477669a839f2f61a00",
+    "url": "/iuo-distancing/static/js/2.eaa2bfc7.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/iuo-distancing/static/js/2.eaa2bfc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824ec06fbd51b80edacf",
+    "url": "/iuo-distancing/static/js/main.86d6f749.chunk.js"
+  },
+  {
+    "revision": "b07568fc83a0f6c31f66",
+    "url": "/iuo-distancing/static/js/runtime-main.e450ba91.js"
+  },
+  {
+    "revision": "d6bc82512a373548bf091277d9cda3bc",
+    "url": "/iuo-distancing/static/media/labels.d6bc8251.yaml"
+  },
+  {
+    "revision": "7b71fdab66181474c013daa3f0361010",
+    "url": "/iuo-distancing/static/media/scene.7b71fdab.yaml"
+  }
+]);
